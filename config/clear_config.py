b6e07b3b46bdb04284f8a5d177e802f6403cfd9c
davidwiygul <david.wiygul@gmail.com>
2020-03-06 21:06:58 -0800

    Update clear_config.py to preserve comments in heirflow ini files

--- config/clear_config.py
+++ config/clear_config.py
@@ -1,10 +1,13 @@
 # clear_config.py
+# called by clear_config to clear all interface.ini values
+# and to clear all multischeduler.ini values other than those in TIMING
+
 
 import configparser
 import sys
 
 path = sys.argv[1]
-config = configparser.ConfigParser(inline_comment_prefixes='#')
+config = configparser.ConfigParser(comment_prefixes='/', allow_no_value=True)
 config.read(path)
 for section in ('DB', 'Q'):
     for key in config[section]:
